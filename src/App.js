9dec810d9e8ed3493648f3a646bd926cb55d1a78
Dtown <dannyotown@gmail.com>
2020-07-30 19:15:43 -0500

    Temporary MVP

--- src/App.js
+++ src/App.js
@@ -78,6 +78,26 @@ function App() {
           onSelectChange={onSelectChange}
         />
       </div>
+      <h5>Rules:</h5>
+      <ol>
+        <li>
+          Any live cell with fewer than two live neighbors dies, as if caused by
+          under-population.
+        </li>{" "}
+        <li>
+          {" "}
+          Any live cell with two or three live neighbors lives on to the next
+          generation.{" "}
+        </li>
+        <li>
+          Any live cell with more than three live neighbors dies, as if by
+          over-population..{" "}
+        </li>
+        <li>
+          Any dead cell with exactly three live neighbors becomes a live cell,
+          as if by reproduction.
+        </li>
+      </ol>
     </>
   );
 }
